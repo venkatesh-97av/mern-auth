fea70c88f1603bbb7196709b3f47601ccd8f2ee7
A.Venkatesh <venkatesh.itechdeveleoper@gmail.com>
2024-05-29 02:52:54 +0530

    create mongodb cluster

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .then((err) => console.log(err));
 
 app.listen(3000, () => {
   console.log("server listening on port: 3000");

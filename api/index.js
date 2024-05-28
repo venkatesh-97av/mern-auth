3c73d277a5b3d8ca19a77dbc8f1f65ea14b6068c
A.Venkatesh <venkatesh.itechdeveleoper@gmail.com>
2024-05-29 03:53:24 +0530

    added middleware fns to handle error and custom error fn

--- api/index.js
+++ api/index.js
@@ -17,6 +17,11 @@ mongoose
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({ success: false, message, statusCode });
+});
 app.listen(3000, () => {
   console.log("server listening on port: 3000");
 });
